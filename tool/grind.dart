057d408523425d569e56baa0d8e2cceb73df2759
patorash <chariderpato@gmail.com>
2017-11-02 03:14:17 +0900

    dart tool/grind.dart でstreamが起動するようになった

--- tool/grind.dart
+++ tool/grind.dart
@@ -1,15 +1,24 @@
+import 'dart:io';
 import 'package:grinder/grinder.dart';
+import '../web/webapp/main.dart' as helloRsp;
 
 main(args) => grind(args);
 
 @Task()
 test() => new TestRunner().testAsync();
 
-@DefaultTask()
-@Depends(test)
-build() {
-  Pub.build();
-}
+// @Depends(test)
+// build() {
+//   Pub.build();
+// }
 
 @Task()
 clean() => defaultClean();
+
+@DefaultTask()
+serve() {
+  Process.start('dart', ['web/webapp/main.dart']).then((Process process) {
+    stdout.addStream(process.stdout);
+    stderr.addStream(process.stderr);
+  });
+}
