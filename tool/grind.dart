86cc2c600477e6d2371a4fb1c56cc1188baea396
patorash <chariderpato@gmail.com>
2017-11-08 00:36:13 +0900

    grinderタスクを整理した。

--- tool/grind.dart
+++ tool/grind.dart
@@ -1,10 +1,7 @@
 import 'dart:io';
-import 'dart:async';
 import 'package:grinder/grinder.dart';
 import 'package:path/path.dart' as path;
 import 'package:watcher/watcher.dart';
-import 'package:stream/rspc.dart' as rspc;
-import 'package:sass/sass.dart' as sass;
 
 main(args) => grind(args);
 
@@ -21,78 +18,7 @@ clean() => defaultClean();
 
 @Task()
 compile() {
-  // pub serveにコンパイルを任せることにしたのでコメントアウトした
-  // dart2js(new Directory(path.absolute('web/client/assets/js')));
-  // sass2css(new Directory(path.absolute('web/client/assets/css')));
-  // compileRsp(new Directory(path.absolute('web/client')));
-
-  // compileRspでコンパイルを行うとエラーが起きる場合があったのでやめた
   runAsync('dart', arguments: ['build.dart']);
-}
-
-/// dartをjsにコンパイルする
-/// 
-/// [file]がDirectoryだった場合は、再帰的に呼び出す。
-/// Fileだったら非同期でコンパイルする。
-void dart2js(FileSystemEntity file) {
-  file.exists().then((bool exists) {
-    if (file is Directory) {
-      Directory dir = file;
-      dir.list().listen((FileSystemEntity fse) {
-        dart2js(fse);
-      });
-    } else if (file is File && file.path.endsWith('.dart')) {
-      runZoned(() => Dart2js.compileAsync(file)
-      , onError: (e) {
-        log(e.toString());
-      });
-    }
-  });
-}
-
-/// sassをcssにコンパイルする
-/// 
-/// [file]がDirectoryだった場合は、再帰的に呼び出す。
-/// Fileだったら非同期でコンパイルする。
-void sass2css(FileSystemEntity file) {
-  file.exists().then((bool exists) {
-    if (file is Directory) {
-      Directory dir = file;
-      dir.list().listen((FileSystemEntity fse) {
-        sass2css(fse);
-      });
-    } else if (file is File &&
-              (file.path.endsWith('.scss') || file.path.endsWith('.sass'))) {
-      runZoned(() => new File("${path.withoutExtension(file.path)}.css").writeAsString(
-          sass.compile(file.path),
-          mode: FileMode.WRITE_ONLY
-        ), onError: (e) {
-        log(e.toString());
-      });
-    }
-  });
-}
-
-/// RSPファイルをコンパイルする
-/// 
-/// [file]がDirectoryだった場合は、再帰的に呼び出す。
-/// Fileだったらコンパイルする。
-void compileRsp(FileSystemEntity file) {
-    file.exists().then((bool exists) {
-    if (file is Directory) {
-      Directory dir = file;
-      dir.list().listen((FileSystemEntity fse) {
-        compileRsp(fse);
-      });
-    } else if (file is File) {
-      if (file.path.endsWith('.rsp.html')) {
-        runZoned(() => rspc.compileFile(file.path)
-        , onError: (e) {
-          log(e.toString());
-        });
-      }
-    }
-  });
 }
 
 @DefaultTask()
@@ -110,13 +36,6 @@ serve() async {
     if (event.path.endsWith('.rsp.html')) {
       runAsync('dart', arguments: ['build.dart']);
     }
-    // pub serveにコンパイルを任せることにしたのでコメントアウトした
-    // if (event.path.endsWith('.dart')) {
-    //   dart2js(new File(event.path));
-    // }
-    // if (event.path.endsWith('.scss') || event.path.endsWith('.sass')) {
-    //   sass2css(new File(event.path));
-    // }
   });
 
   var serverWatcher = new DirectoryWatcher(path.absolute('web/webapp'));
